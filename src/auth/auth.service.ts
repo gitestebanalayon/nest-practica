88f0bc1fcf732492d86fe07292006b3e21b8c7a1
Esteban Alayon <gitesteban0@gmail.com>
2024-08-02 13:56:22 -0400

    Creacion de token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,12 +2,16 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
-import * as bcryptjs from 'bcryptjs'
+import * as bcryptjs from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt'
 
 @Injectable()
 export class AuthService {
   
-  constructor( private readonly usersService: UsersService ){}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService
+  ){}
 
   async register(registerDto: RegisterDto){
     return await this.usersService.create(registerDto)
@@ -26,7 +30,15 @@ export class AuthService {
       throw new UnauthorizedException('Contraseña incorrecta');
     }
 
-    return user
+    const email = user.email
+
+    const payload = { email: email }
+    const token = await this.jwtService.signAsync(payload)
+
+    return {
+      token,
+      email
+    }
 
   }
 
